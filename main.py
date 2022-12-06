20452bfe37d362fa1fcd6b2d88f8d2ef4715d76c
dmp990 <asadali.asif.1@gmail.com>
2022-12-06 23:05:37 +0000

    create generate list fn

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-import pygame
 import random
 
+from information import *
+
 pygame.init()
+
+
+def generate_list(n: int, min_val: int, max_val: int) -> list:
+    """
+    returns a list of n random integers from min_val to max_val
+    """
+    lst = []
+    for _ in range(n):
+        lst.append(random.randint(min_val, max_val))
+    return lst
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+
+    while run:
+        clock.tick(60)
+        
+        for event in pygame.event.get():
+            ...
